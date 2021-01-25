dedc0c180829b893fcb3a2e34cb9a3faa2336454
Pavel Kuznetsov <kpp2508@gmail.com>
2021-01-26 00:16:50 +0300

    3.3. Разбор первой программы. [#456990]

---
+++ src/ru/job4j/Multiple.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args) {
+        // как-то начинал изучать циклы, почему бы не вспомнить как их использовать
+        for (int i = 2; i <= 9; i++) {
+            System.out.println("1 " + "* " + i + " = " + (i));
+        }
+    }
+}
